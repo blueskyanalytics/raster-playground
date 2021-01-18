c8249497fc7ae33ca7829b20a1050b2f26c6524f
Hanumanth P <48819979+vildar@users.noreply.github.com>
2021-01-18 16:49:37 +0530

    switch between basemaps [build deploy]

--- src/components/main-app/ol-init.js
+++ src/components/main-app/ol-init.js
@@ -4,6 +4,9 @@ import 'ol/ol.css';
 import { URL_SHAPE, URL_TILES, URL_COLORS, URL_OPACITY } from 'config';
 import { useQueryParam, StringParam } from 'use-query-params';
 import { usePrevious } from 'react-use';
+import { OSM } from 'ol/source';
+import XYZ from 'ol/source/XYZ';
+import '../../sass/index.sass'
 
 export default function OlInit() {
   const [shape] = useQueryParam(URL_SHAPE, StringParam);
@@ -14,8 +17,24 @@ export default function OlInit() {
   const prevTiles = usePrevious(tiles);
   const prevShape = usePrevious(shape);
 
+  let baseLayer = {}
+  
+  const mapBoxSource = new XYZ({
+    url: `https://api.mapbox.com/styles/v1/srijitcoder/ckhnsil6g15ud19qqo9leet6e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3Jpaml0Y29kZXIiLCJhIjoiY2s3MzhnZGZyMDlrbDNvbW5mcXpwZnoxMyJ9.ILgPQHEJq6lFRt2fN0j2sQ`,
+  })
+  const satelliteSource = new XYZ({
+    url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+  })
+  const osmSource = new OSM()
+
+  const handleBaseMapChange = (source) => {
+    baseLayer.source = source
+    baseLayer.source.refresh()
+  }
+
   useEffect(() => {
     const olInstances = olMain({ shape, tiles, colors, opacity });
+    baseLayer = olInstances.map.getLayers().getArray()[0].values_
 
     if (olInstances.rasterSource && shape && prevTiles !== tiles) {
       olInstances.rasterSource.setUrl(tiles);
@@ -35,10 +54,17 @@ export default function OlInit() {
       olInstances.rasterSource.refresh();
     }
   }, [shape, tiles, colors, opacity, prevTiles, prevShape]);
+
   return (
     <>
       <div>
-        <div id="map" className="main-map"></div>
+        <div id="map" className="main-map">
+          <div className="btn-group">
+            <button className="view" onClick={() => handleBaseMapChange(mapBoxSource)}>MapBox</button>
+            <button className="view" onClick={() => handleBaseMapChange(satelliteSource)}>Satellite</button>
+            <button className="view" onClick={() => handleBaseMapChange(osmSource)}>OSM</button>
+          </div>
+        </div>
       </div>
     </>
   );
