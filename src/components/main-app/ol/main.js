7bca91a9682fcbac298eb353b361fb5408bbaddb
Hanumanth P <48819979+vildar@users.noreply.github.com>
2021-01-19 19:55:28 +0530

    [build deploy]

--- src/components/main-app/ol/main.js
+++ src/components/main-app/ol/main.js
@@ -1,6 +1,5 @@
 import { clipRasterLayer, dragMap, initMap, loadRasterLayer } from './';
-import { getColorsArray, getRgbColorsArray, setMap } from 'utils';
-import { getColorsArray, getRgbColorsArray, addSearchControl } from 'utils';
+import { getColorsArray, getRgbColorsArray, addSearchControl, setMap } from 'utils';
 
 let map = null;
 let rasterSource = null;
