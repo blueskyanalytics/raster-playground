d6a55568d52fdbe435b0a0fc83e08d38e84286e7
Hanumanth P <48819979+vildar@users.noreply.github.com>
2021-01-20 14:30:32 +0530

    Added feature to copy rgba, hsla and hex values

--- src/components/main-app/sidebar.js
+++ src/components/main-app/sidebar.js
@@ -1,5 +1,5 @@
 import { URL_COLORS, URL_OPACITY } from 'config';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { StringParam, useQueryParam } from 'use-query-params';
 import { copyColor, getColorsArray } from 'utils';
 import {
@@ -8,16 +8,29 @@ import {
   ColorsInput,
   AlphaInput,
 } from './sidebar-wrapper';
+import '../../sass/index.sass'
 
 export default function Sidebar() {
   const [colors] = useQueryParam(URL_COLORS, StringParam);
   const [opacity] = useQueryParam(URL_OPACITY, StringParam);
   const [text, setText] = useState('');
-
+  const [colorFormat, setColorFormat] = useState('rgba')
+  const colorText = useRef(null)
+  
   useEffect(() => {
     const colorArray = getColorsArray(colors);
-    setText(copyColor(colorArray, opacity));
-  }, [colors, opacity]);
+    setText(copyColor(colorArray, opacity, colorFormat));
+  }, [colors, opacity, colorFormat]);
+
+  const handleColorFormatChange = event => {
+    setColorFormat(event.target.value)
+  }
+
+  const copyColorFormat = () => {
+    const colorValues = colorText.current
+    colorValues.select()
+    document.execCommand('copy')
+  }
 
   return (
     <div className="sidebar">
@@ -37,7 +50,14 @@ export default function Sidebar() {
         <AlphaInput />
         <br />
         <br />
-        <div>{JSON.stringify(text)}</div>
+        <select id="color-format" defaultValue='rgba' onChange={event => handleColorFormatChange(event)}>
+          <option value='hex'>HEX</option>
+          <option value='rgba'>RGBA</option>
+          <option value='hsla'>HSLA</option>
+        </select>
+        <button className="copy-btn"onClick={copyColorFormat}>Copy</button>
+        <br />
+        <textarea id="text-box" value={JSON.stringify(text)} ref={colorText}></textarea>
       </div>
     </div>
   );
--- src/utils/copy-color.js
+++ src/utils/copy-color.js
@@ -16,9 +16,64 @@ function hexToRgbA(hex, opacity) {
   throw new Error('Bad Hex');
 }
 
-export default function copyColor(colorsArray, opacity) {
+function rgbaToHsla(hex, opacity) {
+  let rgba = hexToRgbA(hex, opacity)
+  let r = rgba.r, g = rgba.g, b = rgba.b, a = rgba.a;
+
+  r /= 255
+  g /= 255
+  b /= 255
+
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+  
+  // Calculate hue
+  // No difference
+  if (delta == 0)
+    h = 0;
+  // Red is max
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  // Green is max
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  // Blue is max
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+    
+  // Make negative hues positive behind 360°
+  if (h < 0)
+      h += 360;
+
+  // Calculate lightness
+  l = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+  // Multiply l and s by 100
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return {
+    h, s, l, a
+  };
+}
+
+export default function copyColor(colorsArray, opacity, text) {
   if (!colorsArray.length) return null;
+  
   let tempColorsArray = [];
-  colorsArray.map(color => tempColorsArray.push(hexToRgbA(color, opacity)));
+  
+  if (text === 'hex') colorsArray.map(color => tempColorsArray.push(color));
+  else if (text === 'rgba') colorsArray.map(color => tempColorsArray.push(hexToRgbA(color, opacity)));
+  else if (text === 'hsla') colorsArray.map(color => tempColorsArray.push(rgbaToHsla(color, opacity)));
+  
   return tempColorsArray;
 }
