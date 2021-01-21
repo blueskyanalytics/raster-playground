5d31b556d22ac95a92ca1a37db08e05acc53bccb
Hanumanth P <48819979+vildar@users.noreply.github.com>
2021-01-21 15:13:08 +0530

    [build deploy]

--- src/components/main-app/themes/global-styles.js
+++ src/components/main-app/themes/global-styles.js
@@ -6,32 +6,37 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
     transition: all 0.5s linear;
   }
+
   #json-pretty > pre::-webkit-scrollbar-thumb{
     background-color: ${({ theme }) => theme.scroll};
   }
+
   #json-pretty > pre{
     background-color: ${({ theme }) => theme.background} !important;
     color: ${({ theme }) => theme.jsonColor} !important;
   }
+
   .__json-key__{
     color: ${({ theme }) => theme.jsonKey} !important;
   } 
+
   .__json-value__{
     color: ${({ theme }) => theme.jsonValue} !important;
   }
+
   .__json-string__{
     color: ${({ theme }) => theme.jsonString} !important;
   }
+
   .input-text {
     background-color: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.text};
   }
+
   .fa.iconButton, #color-format, .copy-btn{
     background-color: ${({ theme }) => theme.btnBgColor};
     color: ${({ theme }) => theme.btnColor};
-  }
-
-  
+  }  
   
   .theme-toggle input:checked + .slider {
     background-color: ${({ theme }) => theme.btnBgColor};
