ca2810bc6a1d7b69278f80df46950dbd2d06685e
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-09-28 22:20:45 +0530

    WIP: fix hidden draggable item issue

--- src/components/main-app/sidebar-wrapper/colors-input.js
+++ src/components/main-app/sidebar-wrapper/colors-input.js
@@ -1,4 +1,6 @@
 import React, { useState, Fragment } from 'react';
+import {SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
+// TODO: remove array-move library
 import { StringParam, useQueryParam } from 'use-query-params';
 import { URL_COLORS, URL_UPDATE_PUSH } from 'config';
 import { ReactComponent as CloseSVG } from 'assets/svg/close.svg';
@@ -25,6 +27,9 @@ const deleteColor = (key, colorsArray, onChangeColor) => {
   }
 };
 
+const SortableItem = SortableElement(({ children }) => children);
+const SortableList = SortableContainer(({ children }) => children);      
+
 export default function ColorsInput() {
   const [colors, onChangeColor] = useQueryParam(URL_COLORS, StringParam);
   const [itemColorStatus, setItemColorStatus] = useState(null);
@@ -44,6 +49,11 @@ export default function ColorsInput() {
     onChangeColor(getColorsString(tempColorsArray), URL_UPDATE_PUSH);
   };
 
+  const sortColor = ({ oldIndex, newIndex }) => {
+    let tempColorsArray = arrayMove(colorsArray, oldIndex, newIndex);
+    onChangeColor(getColorsString(tempColorsArray), URL_UPDATE_PUSH);
+  }
+
   return (
     <>
       <div className="color-row">
@@ -56,31 +66,42 @@ export default function ColorsInput() {
           {' '}
         </a>
       </div>
-      <div className="color-input">
-        {colorsArray.map((color, key) => (
-          <Fragment key={color + key}>
-            <div
-              className="color-input-item"
-              style={
-                itemColorStatus && itemColorStatus.key === key
-                  ? { background: color, borderColor: 'black' }
-                  : { background: color }
-              }
-            >
-              <div
-                className="color-input-item-close"
-                onClick={e => deleteColor(key, colorsArray, onChangeColor)}
-              >
-                <CloseSVG />
-              </div>
-              <div
-                className="color-input-item-overlay"
-                onClick={e => setItemColorStatus({ color, key })}
-              ></div>
-            </div>
-          </Fragment>
-        ))}
-      </div>
+
+        <SortableList
+          axis="xy"
+          lockAxis="xy"
+          distance={1}
+          onSortEnd={sortColor}
+        >
+          <div className="color-input">
+            {colorsArray.map((color, key) => (
+              <SortableItem key={`color-${key}`} index={key}>
+                <Fragment>
+                  <div
+                    className="color-input-item"
+                    style={
+                      itemColorStatus && itemColorStatus.key === key
+                        ? { background: color, borderColor: 'black' }
+                        : { background: color }
+                    }
+                  >
+                    <div
+                      className="color-input-item-close"
+                      onClick={e => deleteColor(key, colorsArray, onChangeColor)}
+                    >
+                      <CloseSVG />
+                    </div>
+                    <div
+                      className="color-input-item-overlay"
+                      onClick={e => setItemColorStatus({ color, key })}
+                    ></div>
+                  </div>
+                </Fragment>
+              </SortableItem>
+            ))}
+          </div>
+        </SortableList>
+
       {itemColorStatus ? (
         <div style={popover}>
           <div
