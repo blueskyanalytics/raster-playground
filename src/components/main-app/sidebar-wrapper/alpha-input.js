1f12b747b9f34c11d7f886dd6f0ed763ea0bdb6b
Lea Franz <lea.franz@edu.fh-joanneum.at>
2021-10-07 21:36:43 +0200

    added slider component with bubble and textfield

--- src/components/main-app/sidebar-wrapper/alpha-input.js
+++ src/components/main-app/sidebar-wrapper/alpha-input.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { URL_OPACITY, URL_UPDATE_PUSH } from 'config';
 import { StringParam, useQueryParam } from 'use-query-params';
 
 export default function AlphaInput() {
   const [opacity, onChangeOpacity] = useQueryParam(URL_OPACITY, StringParam);
 
+  useEffect(() => {
+    const bubbleElement = document.querySelector('.bubble-container');
+    const sliderElement = document.querySelector('.slider-container');
+    const sliderLengthPixel = sliderElement.offsetWidth;
+    bubbleElement.style.left = `${Number(
+      opacity / (1 / (sliderLengthPixel - 20))
+    )}px`;
+  });
+
   return (
     <>
       <p>Alpha Value (0-1)</p>
-      <input
-        type="number"
-        value={opacity}
-        className="input-text"
-        placeholder="Alpha Input"
-        min="0"
-        max="1"
-        step="0.01"
-        onChange={e => onChangeOpacity(e.target.value, URL_UPDATE_PUSH)}
-      />
+      <div className="input-container">
+        <div className="slider-container">
+          <input
+            type="range"
+            min="0"
+            max="1"
+            value={opacity}
+            step="0.01"
+            className="range-slider"
+            onChange={e => onChangeOpacity(e.target.value, URL_UPDATE_PUSH)}
+          />
+          <div className="bubble-container">{opacity}</div>
+        </div>
+        <div className="number-container">
+          <input
+            type="number"
+            value={opacity}
+            className="input-text"
+            placeholder="Alpha Input"
+            min="0"
+            max="1"
+            step="0.01"
+            onChange={e => onChangeOpacity(e.target.value, URL_UPDATE_PUSH)}
+          />
+        </div>
+      </div>
     </>
   );
 }
