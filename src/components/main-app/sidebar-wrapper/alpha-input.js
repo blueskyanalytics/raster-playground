552a1b51fe7b4a35ca7b53414c20650066ae3364
Lea Franz <lea.franz@edu.fh-joanneum.at>
2021-10-08 16:32:34 +0200

    removed bubble, styled textfield

--- src/components/main-app/sidebar-wrapper/alpha-input.js
+++ src/components/main-app/sidebar-wrapper/alpha-input.js
@@ -1,25 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { URL_OPACITY, URL_UPDATE_PUSH } from 'config';
 import { StringParam, useQueryParam } from 'use-query-params';
 
 export default function AlphaInput() {
   const [opacity, onChangeOpacity] = useQueryParam(URL_OPACITY, StringParam);
 
-  useEffect(() => {
-    const bubbleElement = document.querySelector('.bubble-container');
-    const sliderElement = document.querySelector('.slider-container');
-    const sliderLengthPixel = sliderElement.offsetWidth;
-    bubbleElement.style.left = `${Number(
-      opacity / (1 / (sliderLengthPixel - 20))
-    )}px`;
-  });
-
   return (
     <>
       <p>Alpha Value (0-1)</p>
       <div className="input-container">
         <div className="slider-container">
-          <div className="bubble-container">{opacity}</div>
           <input
             type="range"
             min="0"
@@ -30,7 +20,7 @@ export default function AlphaInput() {
             onChange={e => onChangeOpacity(e.target.value, URL_UPDATE_PUSH)}
           />
         </div>
-        <div className="number-container">
+        <div>
           <input
             type="number"
             value={opacity}
