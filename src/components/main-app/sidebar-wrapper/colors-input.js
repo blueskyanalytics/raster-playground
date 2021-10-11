8a0dcdbdd69f025e06b869cf583061c4051f85ad
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-10-11 11:51:33 +0530

    sortable color palletes

--- src/components/main-app/sidebar-wrapper/colors-input.js
+++ src/components/main-app/sidebar-wrapper/colors-input.js
@@ -1,5 +1,9 @@
 import React, { useState, Fragment } from 'react';
-import {SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove,
+} from 'react-sortable-hoc';
 // TODO: remove array-move library
 import { StringParam, useQueryParam } from 'use-query-params';
 import { URL_COLORS, URL_UPDATE_PUSH } from 'config';
@@ -11,6 +15,7 @@ const popover = {
   position: 'absolute',
   zIndex: '3',
 };
+
 const cover = {
   position: 'fixed',
   top: '0px',
@@ -28,7 +33,7 @@ const deleteColor = (key, colorsArray, onChangeColor) => {
 };
 
 const SortableItem = SortableElement(({ children }) => children);
-const SortableList = SortableContainer(({ children }) => children);      
+const SortableList = SortableContainer(({ children }) => children);
 
 export default function ColorsInput() {
   const [colors, onChangeColor] = useQueryParam(URL_COLORS, StringParam);
@@ -52,7 +57,7 @@ export default function ColorsInput() {
   const sortColor = ({ oldIndex, newIndex }) => {
     let tempColorsArray = arrayMove(colorsArray, oldIndex, newIndex);
     onChangeColor(getColorsString(tempColorsArray), URL_UPDATE_PUSH);
-  }
+  };
 
   return (
     <>
@@ -67,40 +72,37 @@ export default function ColorsInput() {
         </a>
       </div>
 
-        <SortableList
-          axis="xy"
-          lockAxis="xy"
-          distance={1}
-          onSortEnd={sortColor}
-        >
-          <div className="color-input">
-            {colorsArray.map((color, key) => (
-              <SortableItem key={`color-${key}`} index={key}>
-                <Fragment>
-                  <div
-                    className="color-input-item"
-                    style={
+      <SortableList axis="xy" distance={1} onSortEnd={sortColor}>
+        <div className="color-input">
+          {colorsArray.map((color, key) => (
+            <SortableItem key={`color-${key}`} index={key}>
+              <Fragment>
+                <div
+                  className="color-input-item draggable"
+                  style={{
+                    background: color,
+                    borderColor:
                       itemColorStatus && itemColorStatus.key === key
-                        ? { background: color, borderColor: 'black' }
-                        : { background: color }
-                    }
+                        ? 'black'
+                        : 'white',
+                  }}
+                >
+                  <div
+                    className="color-input-item-close"
+                    onClick={e => deleteColor(key, colorsArray, onChangeColor)}
                   >
-                    <div
-                      className="color-input-item-close"
-                      onClick={e => deleteColor(key, colorsArray, onChangeColor)}
-                    >
-                      <CloseSVG />
-                    </div>
-                    <div
-                      className="color-input-item-overlay"
-                      onClick={e => setItemColorStatus({ color, key })}
-                    ></div>
+                    <CloseSVG />
                   </div>
-                </Fragment>
-              </SortableItem>
-            ))}
-          </div>
-        </SortableList>
+                  <div
+                    className="color-input-item-overlay"
+                    onClick={e => setItemColorStatus({ color, key })}
+                  ></div>
+                </div>
+              </Fragment>
+            </SortableItem>
+          ))}
+        </div>
+      </SortableList>
 
       {itemColorStatus ? (
         <div style={popover}>
