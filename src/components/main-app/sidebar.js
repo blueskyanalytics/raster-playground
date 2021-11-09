05b36cd2639acf763e22f7450837bc3d2a3d132c
shivamsoni <shivam.spirite3@outlook.com>
2021-11-09 19:04:59 +0530

    make mobile responsive

--- src/components/main-app/sidebar.js
+++ src/components/main-app/sidebar.js
@@ -16,11 +16,15 @@ import JSONPretty from 'react-json-pretty';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import * as JSONPrettyMon from 'react-json-pretty/dist/acai';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export default function Sidebar({ setTheme, theme }) {
   const [colors] = useQueryParam(URL_COLORS, StringParam);
   const [opacity] = useQueryParam(URL_OPACITY, StringParam);
   const [text, setText] = useState('');
   const [colorFormat, setColorFormat] = useState('rgba');
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     const colorArray = getColorsArray(colors);
@@ -33,14 +37,24 @@ export default function Sidebar({ setTheme, theme }) {
     setColorFormat(event.target.value);
   };
 
+  const handleOpen = () => {
+    setToggle(!toggle);
+  };
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyles />
-      <div className="sidebar">
+      <div className={`${toggle === true ? 'sidebarOpen' : 'sidebar'}`}>
         <div className="sidebar-wrapper">
           <h1 className="logo">
             Raster<span>Playground</span>
           </h1>
+
+          <div id="toggle-button">
+            <button onClick={handleOpen} className="button-toggle">
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+          </div>
+
           <div className="theme-switch-wrapper theme-toggle">
             <label
               className="switch"
@@ -60,36 +74,40 @@ export default function Sidebar({ setTheme, theme }) {
               <div className="toggle-moon">🌙</div>
             </label>
           </div>
-          <hr />
-          <ShapeInput />
-          <br />
-          <br />
-          <TilesInput />
-          <br />
-          <br />
-          <ColorsInput />
-          <br />
-          <AlphaInput />
-          <br />
-          <br />
-          <select
-            id="color-format"
-            defaultValue="rgba"
-            onChange={event => handleColorFormatChange(event)}
-          >
-            <option value="hex">HEX</option>
-            <option value="rgba">RGBA</option>
-            <option value="hsla">HSLA</option>
-          </select>
-          <CopyToClipboard text={text}>
-            <button className="copy-btn">Copy</button>
-          </CopyToClipboard>
-          <br />
-          <JSONPretty
-            id="json-pretty"
-            data={text}
-            theme={JSONPrettyMon}
-          ></JSONPretty>
+          {toggle === false && (
+            <>
+              <hr />
+              <ShapeInput />
+              <br />
+              <br />
+              <TilesInput />
+              <br />
+              <br />
+              <ColorsInput />
+              <br />
+              <AlphaInput />
+              <br />
+              <br />
+              <select
+                id="color-format"
+                defaultValue="rgba"
+                onChange={event => handleColorFormatChange(event)}
+              >
+                <option value="hex">HEX</option>
+                <option value="rgba">RGBA</option>
+                <option value="hsla">HSLA</option>
+              </select>
+              <CopyToClipboard text={text}>
+                <button className="copy-btn">Copy</button>
+              </CopyToClipboard>
+              <br />
+              <JSONPretty
+                id="json-pretty"
+                data={text}
+                theme={JSONPrettyMon}
+              ></JSONPretty>
+            </>
+          )}
         </div>
       </div>
     </ThemeProvider>
