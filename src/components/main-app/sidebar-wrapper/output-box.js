eda8352ccb4d475bbb5bd37d7e44936dc161929a
YashasaveeKesarwani99 <yashasvikesarwani1@gmail.com>
2023-06-29 12:13:18 +0530

    more ui changes

--- src/components/main-app/sidebar-wrapper/output-box.js
+++ src/components/main-app/sidebar-wrapper/output-box.js
@@ -5,24 +5,33 @@ import * as JSONPrettyMon from 'react-json-pretty/dist/acai';
 
 const OutputBox = ({ handleColorFormatChange, text }) => {
   return (
-    <div>
-      <select
-        id="color-format"
-        defaultValue="rgba"
-        onChange={event => handleColorFormatChange(event)}
-      >
-        <option value="hex">HEX</option>
-        <option value="rgba">RGBA</option>
-        <option value="hsla">HSLA</option>
-      </select>
-      {/* <CopyToClipboard text={text}>
+    <div className="input-group">
+      <div className="logo-background">
+        <img
+          className=""
+          src={`${window.location.origin}/assets/svg/code.svg`}
+          alt="globe-asia"
+        />
+      </div>
+      <div className="sub-input-group">
+        <select
+          id="color-format"
+          defaultValue="rgba"
+          onChange={event => handleColorFormatChange(event)}
+        >
+          <option value="hex">HEX</option>
+          <option value="rgba">RGBA</option>
+          <option value="hsla">HSLA</option>
+        </select>
+        {/* <CopyToClipboard text={text}>
           <button className="copy-btn">Copy</button>
         </CopyToClipboard> */}
-      <JSONPretty
-        id="json-pretty"
-        data={text}
-        theme={JSONPrettyMon}
-      ></JSONPretty>
+        <JSONPretty
+          id="json-pretty"
+          data={text}
+          theme={JSONPrettyMon}
+        ></JSONPretty>
+      </div>
     </div>
   );
 };
